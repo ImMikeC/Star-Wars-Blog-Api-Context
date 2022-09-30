b404cf2f9577265e05c3d12fdc138e311f4a5e12
hebressus@gmail.com <josemi@debian-BULLSEYE-live-builder-AMD64>
2022-09-30 09:01:58 -0300

    Ajustes y detalles.

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import { ReactDOM, render } from "react-dom";
 
 import 'bootstrap/dist/css/bootstrap.min.css'; // CSS
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 import Layout from "./layout";
 
-ReactDOM.render(<Layout />, document.querySelector("#root"));
+render(<Layout />, document.querySelector("#root"));
--- src/js/layout.js
+++ src/js/layout.js
@@ -8,15 +8,13 @@ import Detail from "./views/Detail.js";
 import injectContext from "./store/appContext";
 
 import Navbar from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
 
-    const basename = process.env.BASENAME || "";
 
     return (
-        <div className="d-flex flex-column">
+        <div className="d-flex">
             <BrowserRouter>
                 <Navbar />
                 <Routes>
